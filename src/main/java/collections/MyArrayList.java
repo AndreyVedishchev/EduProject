b53eb2c32e6d50b3f5bac3f01d8a512a15f7698b
avedishev <anvedischev@vtb.ru>
2024-10-28 19:02:53 +0300

    правка MyArrayList, MyLinkedList 28.10

--- src/main/java/collections/MyArrayList.java
+++ src/main/java/collections/MyArrayList.java
@@ -1,28 +1,30 @@
 package collections;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class MyArrayList {
 
-    private Integer[] arr;
+    private int[] arr;
+    private int capacity;
 
     MyArrayList() {
-        arr = new Integer[5];
+        arr = new int[5];
+        capacity = 0;
     }
 
     void add(int value) {
 
         //если есть место в существующем массиве, то добавляем в него и выходим
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == null) {
+            if (arr[i] == 0 && capacity == i) {
                 arr[i] = value;
+                capacity++;
                 return;
             }
         }
 
         //если места не нашлось, создаем новый в 2 раза больше, переливаем все в него и после добавляем новое значение
-        Integer[] newArr = new Integer[arr.length*2];
+        int[] newArr = new int[arr.length*2];
         for (int i = 0; i < arr.length; i++) {
             newArr[i] = arr[i];
         }
@@ -31,17 +33,25 @@ public class MyArrayList {
     }
 
     void remove(int index) {
-        arr[index] = null;
+        if (index > capacity-1 || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        for (int i = index; i < capacity; i++) {
+            arr[i] = arr[i+1];
+        }
+        capacity--;
     }
 
     int get(int index) {
+        if (index > capacity-1 || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
         return arr[index];
     }
 
     boolean contains(int value) {
-        Integer val = value;
-        for (Integer i : arr) {
-            if (i == val) {
+        for (int i = 0; i < capacity; i++) {
+            if (arr[i] == value) {
                 return true;
             }
         }
@@ -49,26 +59,31 @@ public class MyArrayList {
     }
 
     int size() {
-        int cnt = 0;
-        for (Integer i : arr) {
-            if (i != null) {
-                cnt++;
-            }
-        }
-        return cnt;
+        return capacity;
     }
 
-    void print() {
-        Arrays.stream(arr).filter(Objects::nonNull).forEach(System.out::println);
+    @Override
+    public String toString() {
+        return Arrays.toString(arr) + " size: " + size();
     }
 
     public static void main(String[] args) {
         MyArrayList ml = new MyArrayList();
         ml.add(3);
-        ml.add(4);
+        ml.add(0);
         ml.add(5);
         ml.add(6);
+        ml.add(7);
+        ml.add(8);
 
-        ml.print();
+        System.out.println(ml);
+        System.out.println(ml.get(5));
+
+        ml.remove(2);
+        System.out.println(ml);
+
+        ml.add(9);
+        System.out.println(ml);
+        System.out.println(ml.contains(1));
     }
 }
--- src/main/java/collections/MyQueue.java
+++ src/main/java/collections/MyQueue.java
@@ -1,81 +1,97 @@
 package collections;
 
+import java.util.Arrays;
+
 public class MyQueue {
 
-    private int len = 5;
-    private int[] arr = new int[len];
-    private int in = -1;
-    private int out = -1;
+    private int[] arr = new int[5];
+    private int rear = -1;
+    private int front = -1;
+
 
     public boolean add(int val) {
         if (isFull()) {
             return false;
         } else if (isEmpty()) {
-            in = out = 0;
-            arr[in] = val;
+            rear = front = 0;
+            arr[rear] = val;
             return true;
         } else {
-            in++;
-            arr[in] = val;
-            return true;
+            //если впереди есть место, то сначала добавляю вперед
+            if (front > 0) {
+                arr[--front] = val;
+                return true;
+            //иначе добавляю в конец
+            } else {
+                arr[++rear] = val;
+                return true;
+            }
         }
     }
 
     public boolean remove() {
         if (isEmpty()) {
             return false;
-        } else if (in == 0 && out == 0) {
-            in = out = -1;
+        }
+        else if (rear == 0 && front == 0) {
+            arr[front] = 0;
+            rear = front = -1;
             return true;
-        } else {
-            out++;
-            return false;
+        }
+        else {
+            arr[front++] = 0;
+            return true;
         }
     }
 
     public int peek() {
-        return arr[out];
+        return arr[front];
     }
 
     public boolean isEmpty() {
-        if (in == -1 && out == -1) {
+        if (rear == -1 && front == -1) {
             return true;
         }
         return false;
     }
 
     public boolean isFull() {
-        if (in == len - 1) {
-            return true;
+        if (rear == arr.length-1 && front == 0) {
+            throw new IllegalArgumentException();
         }
         return false;
     }
 
+    @Override
+    public String toString() {
+        return "front: " + front + " " + Arrays.toString(arr) + " rear: " + rear;
+    }
+
     public static void main(String[] args) {
+
         MyQueue myQueue = new MyQueue();
-        System.out.println("back: " + myQueue.in);
-        System.out.println("front: " + myQueue.out);
-        System.out.println("------------------");
-
-        myQueue.add(3);
-        myQueue.add(2);
-        myQueue.add(1);
-        myQueue.add(11);
-        System.out.println("добавил 4");
-
-        System.out.println("------------------");
-        System.out.println("back: " + myQueue.in);
-        System.out.println("front: " + myQueue.out);
-
-        System.out.println("------------------");
+        myQueue.add(10);
+        myQueue.add(20);
+        myQueue.add(30);
+        System.out.println(myQueue);
+        myQueue.add(40);
+        myQueue.add(50);
+        System.out.println(myQueue);
         myQueue.remove();
-        System.out.println("удалил 1");
+        System.out.println(myQueue);
         myQueue.remove();
-        System.out.println("удалил 1");
+        System.out.println(myQueue);
 
-        System.out.println("------------------");
-        System.out.println("back: " + myQueue.in);
-        System.out.println("front: " + myQueue.out);
-
+        myQueue.add(60);
+        System.out.println(myQueue);
+//
+//        myQueue.remove();
+//        System.out.println(myQueue);
+//
+//        myQueue.add(70);
+//        System.out.println(myQueue);
+//
+//        myQueue.add(80);
+//        System.out.println(myQueue);
     }
 }
